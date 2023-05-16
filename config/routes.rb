f4c5df6aa0a0425771311df48a3fe430ff6719c6
Raphael Zimeray <raphaelzimeray@me.com>
2023-05-16 19:12:37 +0200

    views and rooting of index and new teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root "teams#index"
+  resources :teams do
+    collection do
+      post :generate
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
