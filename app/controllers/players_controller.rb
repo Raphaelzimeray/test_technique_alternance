0a60665142a63c764096f8e3cc9573571e29265d
Raphael Zimeray <raphaelzimeray@me.com>
2023-05-17 14:33:33 +0200

    controllers update

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,32 +1,55 @@
 class PlayersController < ApplicationController
-
+  before_action :set_team
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
 
   def index
-    @players = Player.all
+    @players = @team.players
   end
 
   def show
-    @player = Player.find(params[:id])
   end
 
   def new
-    @player = Player.new
+    @player = @team.players.build
   end
 
   def create
-    @player = Player.create(player_params)
+    @player = @team.players.build(player_params)
     if @player.save
-      redirect_to player_path(@player)
+      redirect_to [@team, @player], notice: 'Joueur créé.'
     else
-      render :new, alert: "Error"
+      flash.now[:alert] = @player.errors.full_messages.join(', ')
+      render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @player.update(player_params)
+      redirect_to [@team, @player], notice: 'Player was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @player.destroy
+    redirect_to team_players_url(@team), notice: 'Player was successfully destroyed.'
+  end
 
   private
 
-  def player_params
-    params.require(:player).permit(:name, :team, :role)
+  def set_team
+    @team = Team.find(params[:team_id])
   end
 
+  def set_player
+    @player = @team.players.find(params[:id])
+  end
+
+  def player_params
+    params.require(:player).permit(:name, :role)
+  end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,12 +1,6 @@
 class TeamsController < ApplicationController
-
-
   def index
-    @team = Team.all
-  end
-
-  def show
-    @team = Team.find(params[:id])
+    @teams = Team.all
   end
 
 
@@ -15,27 +9,21 @@ class TeamsController < ApplicationController
   end
 
   def create
-    @team = Team.create(teams_params)
-      if @team.save
-        redirect_to  team_path(@team)
-      else
-        render :new
-      end
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to @team, notice: 'Equipe créée.'
+    else
+      render 'new'
+    end
   end
 
-  def generate
-    8.times do |i|
-      team = Team.create(name: "Team #{i + 1}", city: "City #{i + 1}")
-      11.times do |j|
-        team.players.create(name: "Player #{j + 1}", role: ['Heal', 'Tank', 'DPS'].sample)
-      end
-    end
-    redirect_to teams_path, notice: '8 teams with 11 players each have been generated.'
+  def show
+    @team = Team.find(params[:id])
   end
 
   private
 
-  def teams_params
+  def team_params
     params.require(:team).permit(:name, :city)
   end
 end
